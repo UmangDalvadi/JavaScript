0235e81a594f8cf11b0b25e21eece6cda48ae84e
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-06 13:28:02 +0530

    add

--- practicle02.js
+++ practicle02.js
@@ -1,29 +1,44 @@
 //pro 01
 
-let mark={
-    umang:99,
-    vikash:54,
-    keval:4,
-    dhruv:84
+let mark = {
+    umang: 99,
+    vikash: 54,
+    keval: 4,
+    dhruv: 84
 }
 
-for(let i=0;i<Object.keys(mark).length;i++)
-{
-    console.log("mark of "+ Object.keys(mark)[i]+" is " +mark[Object.keys(mark)[i]])
-}
-
-console.log("\n")
-
-for(let i in mark)
-{
-    console.log("mark of "+i+" is "+mark[i])
+for (let i = 0; i < Object.keys(mark).length; i++) {
+    console.log("mark of " + Object.keys(mark)[i] + " is " + mark[Object.keys(mark)[i]])
 }
 
 console.log("\n")
 
-
-for(let i of Object.keys(mark))
-{
-    console.log("mark of "+i+" is "+mark[i])
+for (let i in mark) {
+    console.log("mark of " + i + " is " + mark[i])
 }
+
+console.log("\n")
+
+
+for (let i of Object.keys(mark)) {
+    console.log("mark of " + i + " is " + mark[i])
+}
+
+
+//pro 03
+
+console.log("\n")
+
+let validnum = 5
+const prompt = require("prompt-sync")();
+let num
+
+while (num != validnum) {
+    num = prompt("enter number : ")
+    if (num != validnum) {
+        console.log("try again")
+    }
+}
+
+console.log("valid!!")
 
