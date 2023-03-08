42776a479528a0d6f7db8f3d2b2312b21911a364
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-09 01:06:52 +0530

    add

---
+++ dommanip.js
@@ -0,0 +1,15 @@
+let main = document.getElementById('main')
+console.log(main)
+console.log(main.innerHTML)
+// main.innerHTML = "< li > elment2</li>"
+// console.log(main.innerHTML)
+
+
+let conatainer = document.getElementsByClassName('conatainer')
+console.log(conatainer)
+
+let select = document.querySelectorAll('#mainui>li')
+console.log(select)
+console.log(select[0].innerHTML)
+console.log(select[1].innerHTML)
+
