a51a0431367fee12f974bfa2984f2b76eac9f391
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-09 17:38:40 +0530

    add

---
+++ date.js
@@ -0,0 +1,15 @@
+let date = new Date()
+console.log(date)
+
+let date0 = new Date(1000)
+console.log(date0)
+
+let date1 = new Date(0)//refer time
+console.log(date1)
+
+let date2 = Date.now()//time show in milisecond from refer time
+console.log(date2)
+
+let bdate = Date(2004, 04, 24, 02, 15, 24)
+console.log(bdate)
+
